fe387c846ef1b58ef720ba91f922c2a7a45acefd
GiorgosNik <mirandagioman@gmail.com>
2022-05-02 15:57:34 +0300

    Browse front-end improvements

--- ui/src/components/Browse.js
+++ ui/src/components/Browse.js
@@ -13,6 +13,9 @@ import {
   SliderTrack,
   SliderFilledTrack,
   SliderThumb,
+  SimpleGrid,
+  LinkBox,
+  LinkOverlay,
   SliderMark,
   Input,
   Center,
@@ -26,19 +29,77 @@ import Axios from "axios";
 import { Container, Row, Col } from "react-grid-system";
 const IMAGE =
   "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80";
+const goToAuctionPage = (id) => {
+  window.location.href = "/auction/" + id;
+};
 
+const producsPerPage = 6;
 export default function Browse() {
+  const [curPage, setCurPage] = useState(1);
+
+  const pageSelectHandler = (pageId) => {
+    setCurPage(pageId);
+  };
+  var productArray = [];
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+  productArray.push(1);
+
+  var pagesArray = [];
+  for (let i = 0; i < productArray.length / producsPerPage; i++) {
+    pagesArray.push(1);
+  }
+  console.log(pagesArray.length);
   return (
     <Box m={5}>
       <Container>
         <Row>
-          <Col sm={4}>
+          <Col sm={2.5}>
             <Filters />
+            {pagesArray.map((page, index) => {
+              return (
+                <Button
+                  key={index}
+                  marginTop={40}
+                  mx={1}
+                  colorScheme="purple"
+                  variant="outline"
+                  onClick={() => pageSelectHandler(index + 1)}
+                >
+                  {index + 1}
+                </Button>
+              );
+            })}
           </Col>
-          <Col sm={4}>
-            <ProductCard />
-          </Col>
-          <Col sm={4}>One of three columns</Col>
+          <SimpleGrid columns={[1, 2, 3]} spacing={10}>
+            {productArray.map((category, index) => {
+              if (
+                index > producsPerPage * curPage - producsPerPage &&
+                index <= producsPerPage * curPage
+              )
+                return (
+                  <ProductCard
+                    key={index}
+                    productName={"name"}
+                    sellerUsername={"seller name"}
+                    price={"price"}
+                    buyoutPrice={"buyout price"}
+                    id={index}
+                  />
+                );
+            })}
+          </SimpleGrid>
         </Row>
       </Container>
     </Box>
@@ -176,68 +237,82 @@ function PriceSlider() {
   );
 }
 
-function ProductCard() {
+function ProductCard({
+  productName,
+  sellerUsername,
+  price,
+  buyoutPrice,
+  id,
+  index,
+}) {
   return (
-    <Center py={12}>
-      <Box
-        role={"group"}
-        p={6}
-        maxW={"330px"}
-        w={"full"}
-        bg={useColorModeValue("white", "gray.800")}
-        boxShadow={"2xl"}
-        rounded={"lg"}
-        pos={"relative"}
-        zIndex={1}
-      >
-        <Box
-          rounded={"lg"}
-          mt={-12}
-          pos={"relative"}
-          height={"230px"}
-          _after={{
-            transition: "all .3s ease",
-            content: '""',
-            w: "full",
-            h: "full",
-            pos: "absolute",
-            top: 5,
-            left: 0,
-            backgroundImage: `url(${IMAGE})`,
-            filter: "blur(15px)",
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: "blur(20px)",
-            },
-          }}
-        >
-          <Image
+    <LinkBox as="article" maxW="sm" p="5" borderWidth="0px" rounded="md">
+      <LinkOverlay onClick={() => goToAuctionPage(id)}>
+        <Center py={12}>
+          <Box
+            role={"group"}
+            style={{ zIndex: "0" }}
+            p={6}
+            maxW={"330px"}
+            w={"full"}
+            bg={useColorModeValue("white", "gray.800")}
+            boxShadow={"2xl"}
             rounded={"lg"}
-            height={230}
-            width={282}
-            objectFit={"cover"}
-            src={IMAGE}
-          />
-        </Box>
-        <Stack pt={10} align={"center"}>
-          <Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
-            Brand
-          </Text>
-          <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
-            Nice Chair, pink
-          </Heading>
-          <Stack direction={"row"} align={"center"}>
-            <Text fontWeight={800} fontSize={"xl"}>
-              $57
-            </Text>
-            <Text textDecoration={"line-through"} color={"gray.600"}>
-              $199
-            </Text>
-          </Stack>
-        </Stack>
-      </Box>
-    </Center>
+            pos={"relative"}
+            zIndex={1}
+          >
+            <Box
+              rounded={"lg"}
+              mt={-12}
+              pos={"relative"}
+              height={"230px"}
+              _after={{
+                transition: "all .3s ease",
+                content: '""',
+                w: "full",
+                h: "full",
+                pos: "absolute",
+                top: 5,
+                left: 0,
+                backgroundImage: `url(${IMAGE})`,
+                filter: "blur(15px)",
+                zIndex: -1,
+              }}
+              _groupHover={{
+                _after: {
+                  filter: "blur(20px)",
+                },
+              }}
+            >
+              <Image
+                rounded={"lg"}
+                height={230}
+                width={282}
+                objectFit={"cover"}
+                src={IMAGE}
+              />
+            </Box>
+            <Stack pt={10} align={"center"}>
+              <Text
+                color={"gray.500"}
+                fontSize={"sm"}
+                textTransform={"uppercase"}
+              >
+                {sellerUsername + " " + id}
+              </Text>
+              <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
+                {productName}
+              </Heading>
+              <Stack direction={"row"} align={"center"}>
+                <Text fontWeight={800} fontSize={"xl"}>
+                  {price}
+                </Text>
+                <Text color={"gray.600"}>{buyoutPrice}</Text>
+              </Stack>
+            </Stack>
+          </Box>
+        </Center>
+      </LinkOverlay>
+    </LinkBox>
   );
 }
