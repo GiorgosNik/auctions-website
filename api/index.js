c86824383f42d9646832a9d085c7a4573808e8ef
nefelitav <nefeli.tavoulari123@gmail.com>
2022-05-04 02:26:59 +0300

    add code for https

--- api/index.js
+++ api/index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
+const https = require("https");
+const fs = require("fs");
 const auction = require("./modules/Auction");
 const auth = require("./modules/Auth");
 const bid = require("./modules/Bid");
 const category = require("./modules/Category");
 const message = require("./modules/Message");
 const newsletter = require("./modules/Newsletter");
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "cert.pem")),
+};
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +26,8 @@ app.use("/category", category);
 app.use("/messaging", message);
 app.use("/newsletter", newsletter);
 app.use("/images", express.static(path.join(__dirname, "./images")));
+
+// https.createServer(options, app).listen(5000);
 
 app.listen(5000, () => {
   console.log("API listening at http://localhost:5000");
